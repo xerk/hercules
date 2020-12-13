c2db5eb18cbf3324cb8fec67a43be1606b9251af
Ahmed Mamdouh <gm.xerk@gmail.com>
2020-12-14 01:31:30 +0200

    Change clients table

--- database/migrations/2020_10_09_164950_create_clients_table.php
+++ database/migrations/2020_10_09_164950_create_clients_table.php
@@ -17,19 +17,25 @@ class CreateClientsTable extends Migration
             $table->id();
             $table->bigInteger('unique_id')->nullable();
             $table->string('mobile', 20)->nullable();
-            // $table->string('email')->nullable();
-            // $table->string('name')->nullable();
-            // $table->string('username')->nullable();
-            // $table->string('religion')->nullable();
-            // $table->string('birthday')->nullable();
-            // $table->string('work')->nullable();
-            // $table->string('position')->nullable();
-            // $table->string('hometown')->nullable();
-            // $table->string('location')->nullable();
-            // $table->string('education')->nullable();
-            // $table->string('relationship')->nullable();
+            $table->string('email', 45)->nullable();
+            $table->string('name', 100)->nullable();
+            $table->string('username')->nullable();
+            $table->string('religion', 45)->nullable();
+            $table->string('birthday', 45)->nullable();
+            $table->string('work')->nullable();
+            $table->string('position')->nullable();
+            $table->string('hometown')->nullable();
+            $table->string('location')->nullable();
+            $table->string('education')->nullable();
+            $table->string('relationship')->nullable();
             $table->string('nationality', 20)->nullable();
 
+            $table->index(['unique_id',
+                               'religion',
+                               'work',
+                               'position',
+                               'education',
+                               'relationship', 'hometown']);
             // Timestamps
             $table->timestamps();
         });
