07ff545fb16fe44349e0d4a859c68ab13e7558b3
xerk <gm.xerk@gmail.com>
2021-05-06 22:46:26 +0200

    asdasd

--- database/migrations/2021_05_06_191949_add_filed_to_client_user_table.php
+++ database/migrations/2021_05_06_191949_add_filed_to_client_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFiledToClientsTable extends Migration
+class AddFiledToClientUserTable extends Migration
 {
     /**
      * Run the migrations.
