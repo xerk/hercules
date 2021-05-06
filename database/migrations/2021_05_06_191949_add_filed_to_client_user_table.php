701cc8d18e84c2c5f32f036080d2ae0678e13bed
xerk <gm.xerk@gmail.com>
2021-05-06 22:44:54 +0200

    asd

---
+++ database/migrations/2021_05_06_191949_add_filed_to_client_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFiledToClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client_user', function (Blueprint $table) {
+            $table->index('status');
+            $table->index('count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('client_user', function (Blueprint $table) {
+            $table->dropColumn('status', 'count');
+        });
+    }
+}
